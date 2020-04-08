b389caa8aeeaac6c7406f84fbae8ff79379aa79f
a-kv <aakvasovaa@gmail.com>
2020-04-08 09:46:17 +0300

    task 2

--- src/Components/Avatar/Avatar.jsx
+++ src/Components/Avatar/Avatar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Avatar.css';
+import classes from './Avatar.module.css';
 
 const Avatar = (props) => {
        return (
-        <div className="avatar">
+        <div className={classes.avatar}>
           <img src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png"/>
 
         </div>
--- src/Components/TextMessage/TextMessage.jsx
+++ src/Components/TextMessage/TextMessage.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import './TextMessage.css';
+import classes from './TextMessage.module.css';
 
 const TextMessage = (props) => {
     let date = new Date();
-       return (
-        <div className="back">
-            <div className="name">Kvasova Anna</div>
-           <div className="textMessage">I`m your worst nightmare!</div>
-            <div className="time">{date.toLocaleTimeString('ru-RU', {hour: '2-digit', minute: '2-digit'})}
+    return (
+        <div className={classes.message}>
+
+            <div className={classes.back}>
+                <div className={classes.name}>Kvasova Anna</div>
+                <div className={classes.textMessage}>I`m your worst nightmare! I`m your worst nightmare! I`m your worst
+                    nightmare!
+                </div>
+                <div className={classes.time}>{date.toLocaleTimeString('ru-RU', {hour: '2-digit', minute: '2-digit'})}
+                </div>
             </div>
-        </div>
+            </div>
+
     );
 }
 
